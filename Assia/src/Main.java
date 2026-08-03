cdc6d885c6cabf0f4f9b11eca69224a93d989aaa
ASSIA BENMAZOUZ <asia94495@gmail.com>
2026-08-03 19:01:39 -0400

    Initialisation du projet Java

---
+++ Assia/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bibliothèque numérique UA3");
+        System.out.println("Application démarrée avec succès.");
+    }
+}
